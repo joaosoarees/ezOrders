b8439f6e253e878515a90a51a777e6ecb8668289
João Vitor <jsoareez@gmail.com>
2021-01-10 21:18:58 -0300

    create mongoose connection and OrderSchema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
+mongoose.connect('mongodb://localhost:27017/ezorders', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
